ff97c1a304da61e948110905b570286a6921e720
Garett Arrowood <garettarrowood@gmail.com>
2015-12-22 11:39:29 -0500

    Scoped ships

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,4 +5,23 @@ class Board < ApplicationRecord
 
   validates_associated :game
 
+  def patrol_boat
+    ships.where(classification: "Patrol Boat")[0]
+  end
+
+  def destroyer
+    ships.where(classification: "Destroyer")[0]
+  end
+
+  def submarine
+    ships.where(classification: "Submarine")[0]
+  end
+
+  def battleship
+    ships.where(classification: "Battleship")[0]
+  end
+
+  def aircraft_carrier
+    ships.where(classification: "Aircraft Carrier")[0]
+  end
 end
