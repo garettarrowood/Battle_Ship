1efb714cfb2eb28ec342fcab84dac44c6704272c
Garett Arrowood <garettarrowood@gmail.com>
2016-01-06 21:08:01 -0500

    Merge and specify board test

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -3,11 +3,8 @@ require 'rails_helper'
 RSpec.describe Board, :type => :model do
   let(:board) { create :board }
   
-  it 'has valid factories' do
+  it 'has valid factories with 5 placed ships' do
     expect(board).to be_valid
-  end
-
-  it 'intializes with 5 placed ships' do
     expect(board.ships.size).to eq 5
   end
 
