ef33c992c54fecae78748e02fc6729bd4f3467ae
Garett Arrowood <garettarrowood@gmail.com>
2016-01-09 19:46:29 -0500

    Add service model specs

---
+++ spec/services/generate_random_ships_spec.rb
@@ -0,0 +1,138 @@
+require 'rails_helper'
+require_relative '../../app/models/services/generate_random_ships.rb'
+
+RSpec.describe GenerateRandomShips, :type => :model do
+  let(:board) { create :board, ships: [] }
+
+  before(:each) do
+    @random_setup = GenerateRandomShips.new(board)
+  end
+
+  it "initializes with board" do
+    expect(@random_setup).to be_instance_of(GenerateRandomShips)
+  end
+
+  context "#place_aircraft_carrier" do
+    it "adds ship with classification 'Aircraft Carrier' of 5 positions to board" do
+      expect(board.ships.length).to eq(0)
+      @random_setup.place_aircraft_carrier
+      expect(board.ships.length).to eq(1)
+      expect(board.ships[0].classification).to eq("Aircraft Carrier")
+      expect(board.ships[0].positions.length).to eq(5)
+    end
+
+    it "all ship positions are unique" do
+      @random_setup.place_aircraft_carrier
+      expect(board.occupied_positions.uniq.length).to eq(5)
+    end
+  end
+
+  context "#place_battleship" do
+    before(:each) do
+      @random_setup.place_aircraft_carrier
+    end
+
+    it "adds ship with classification 'Battleship' of 4 positions to board" do
+      expect(board.ships.length).to eq(1)
+      @random_setup.place_battleship
+      expect(board.ships.length).to eq(2)
+      expect(board.ships[1].classification).to eq("Battleship")
+      expect(board.ships[1].positions.length).to eq(4)
+    end
+
+    it "adds 4 more occupied_positions to board" do
+      expect { @random_setup.place_battleship }.to change{board.occupied_positions.length}.from(5).to(9)
+    end
+
+    it "does not add any overlapping positions" do
+      @random_setup.place_battleship
+      expect(board.occupied_positions.uniq.length).to eq(9)
+    end
+  end
+
+  context "#place_submarine" do
+    before(:each) do
+      @random_setup.place_aircraft_carrier
+      @random_setup.place_battleship
+    end
+
+    it "adds ship with classification 'Submarine' of 3 positions to board" do
+      expect(board.ships.length).to eq(2)
+      @random_setup.place_submarine
+      expect(board.ships.length).to eq(3)
+      expect(board.ships[2].classification).to eq("Submarine")
+      expect(board.ships[2].positions.length).to eq(3)
+    end
+
+    it "adds 4 more occupied_positions to board" do
+      expect { @random_setup.place_submarine }.to change{board.occupied_positions.length}.from(9).to(12)
+    end
+
+    it "does not add any overlapping positions" do
+      @random_setup.place_submarine
+      expect(board.occupied_positions.uniq.length).to eq(12)
+    end
+  end
+
+  context "#place_destroyer" do
+    before(:each) do
+      @random_setup.place_aircraft_carrier
+      @random_setup.place_battleship
+      @random_setup.place_submarine
+    end
+
+    it "adds ship with classification 'Destroyer' of 3 positions to board" do
+      expect(board.ships.length).to eq(3)
+      @random_setup.place_destroyer
+      expect(board.ships.length).to eq(4)
+      expect(board.ships[3].classification).to eq("Destroyer")
+      expect(board.ships[3].positions.length).to eq(3)
+    end
+
+    it "adds 4 more occupied_positions to board" do
+      expect { @random_setup.place_destroyer }.to change{board.occupied_positions.length}.from(12).to(15)
+    end
+
+    it "does not add any overlapping positions" do
+      @random_setup.place_destroyer
+      expect(board.occupied_positions.uniq.length).to eq(15)
+    end
+  end
+
+  context "#place_patrol_boat" do
+    before(:each) do
+      @random_setup.place_aircraft_carrier
+      @random_setup.place_battleship
+      @random_setup.place_submarine
+      @random_setup.place_destroyer
+    end
+
+    it "adds ship with classification 'Patrol Boat' of 2 positions to board" do
+      expect(board.ships.length).to eq(4)
+      @random_setup.place_patrol_boat
+      expect(board.ships.length).to eq(5)
+      expect(board.ships[4].classification).to eq("Patrol Boat")
+      expect(board.ships[4].positions.length).to eq(2)
+    end
+
+    it "adds 4 more occupied_positions to board" do
+      expect { @random_setup.place_patrol_boat }.to change{board.occupied_positions.length}.from(15).to(17)
+    end
+
+    it "does not add any overlapping positions" do
+      @random_setup.place_patrol_boat
+      expect(board.occupied_positions.uniq.length).to eq(17)
+    end
+  end
+
+  context "#run!" do 
+    it "should receive all place_{ship} methods" do
+      expect(@random_setup).to receive(:place_aircraft_carrier)
+      expect(@random_setup).to receive(:place_battleship)
+      expect(@random_setup).to receive(:place_submarine)
+      expect(@random_setup).to receive(:place_destroyer)
+      expect(@random_setup).to receive(:place_patrol_boat)
+      @random_setup.run!
+    end
+  end
+end
---
+++ spec/services/setup_new_game_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+require_relative '../../app/models/services/setup_new_game.rb'
+
+RSpec.describe SetupNewGame, :type => :model do
+  let(:game) { create :game }
+  json = {"0"=>{"length"=>"2", "classification"=>"patrol-boat", "positions"=>["3-5", "3-6"]}, "1"=>{"length"=>"3", "classification"=>"destroyer", "positions"=>["4-5", "4-6", "4-7"]}, "2"=>{"length"=>"3", "classification"=>"submarine", "positions"=>["6-4", "6-5", "6-6"]}, "3"=>{"length"=>"4", "classification"=>"battleship", "positions"=>["8-6", "8-7", "8-8", "8-9"]}, "4"=>{"length"=>"5", "classification"=>"aircraft-carrier", "positions"=>["10-4", "10-5", "10-6", "10-7", "10-8"]}}
+
+  before(:each) do
+    @game_setup = SetupNewGame.new(json, game)
+  end
+
+  it "initializes with json and game" do
+    expect(@game_setup).to be_instance_of(SetupNewGame)
+  end
+
+  it "parses and gathers ship positions upon initialization" do
+    expect(@game_setup.patrol_boat).to eq(["3-5", "3-6"])
+    expect(@game_setup.destroyer).to eq(["4-5", "4-6", "4-7"])
+    expect(@game_setup.submarine).to eq(["6-4", "6-5", "6-6"])
+    expect(@game_setup.battleship).to eq(["8-6", "8-7", "8-8", "8-9"])
+    expect(@game_setup.aircraft_carrier).to eq(["10-4", "10-5", "10-6", "10-7", "10-8"])
+  end
+
+  context "#strings_to_points" do
+    before(:each) do
+      @positions = @game_setup.patrol_boat
+      @points = @game_setup.strings_to_points(@positions)
+    end
+
+    it "returns same length array as argument" do
+      expect(@positions.length).to eq(@points.length)
+    end
+
+    it "morphs String positions into ActiveRecord::Points" do
+      expect(@positions[0]).to be_instance_of(String)
+      expect(@points[0]).to be_instance_of(ActiveRecord::Point)
+    end
+
+    it "changes 1st position number to y coordinate" do
+      first_num = @positions[0].scan(/\d/)[0].to_i
+      expect(@points[0].y).to eq(first_num)
+    end
+
+    it "changes 2nd position number to x coordinate" do
+      second_num = @positions[0].scan(/\d/)[1].to_i
+      expect(@points[0].x).to eq(second_num)
+    end
+  end
+
+  context "#create_user_ships" do
+    before(:each) do
+      @board = game.boards.create(opponent?: false)
+      @game_setup.create_user_ships(@board)
+    end
+
+    it "adds 5 ships to board" do
+      expect(@board.ships.length).to eq(5)
+      expect(@board.ships[0]).to be_instance_of(Ship)
+    end
+
+    it "only adds 1 of each classification of ship" do
+      expect(@board.ships[0].classification).to eq("Patrol Boat")
+      expect(@board.ships[1].classification).to eq("Destroyer")
+      expect(@board.ships[2].classification).to eq("Submarine")
+      expect(@board.ships[3].classification).to eq("Battleship")
+      expect(@board.ships[4].classification).to eq("Aircraft Carrier")
+    end
+  end
+end
