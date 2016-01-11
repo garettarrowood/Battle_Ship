c3cf41daa37c789bcff01987fae7b554a47a45c3
Garett Arrowood <garettarrowood@gmail.com>
2016-01-10 21:13:55 -0500

    Instance objects for erb views and for gon in GamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
 
   def index
+    @game = current_user.games.last
   end
 
   def new
@@ -8,12 +9,15 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = current_user.games.create()
+    @game = current_user.games.create
     SetupNewGame.new(params[:ships], @game).run!
     render js: "window.location = '/games/#{@game.id}'"
   end
 
   def show
+    @opponent_board = Game.find(params[:id]).boards.where(opponent?: true)[0]
+    @user_board = Game.find(params[:id]).boards.where(opponent?: false)[0]
+    gon.jbuilder
   end
 
 end
