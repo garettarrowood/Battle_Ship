52b0a580827225538109a0a53a0e8db39b33ca61
Garett Arrowood <garettarrowood@gmail.com>
2016-01-18 14:30:10 -0500

    Delete unused visible? method tests

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -8,28 +8,4 @@ RSpec.describe Board, :type => :model do
     expect(board.ships.size).to eq 5
   end
 
-  context 'belonging to player' do 
-    let(:player_board) { create :board, opponent?: false }
-
-    it 'holds visible ships once placed' do
-      expect(player_board.patrol_boat.visible?).to eq true
-      expect(player_board.destroyer.visible?).to eq true
-      expect(player_board.submarine.visible?).to eq true
-      expect(player_board.battleship.visible?).to eq true
-      expect(player_board.aircraft_carrier.visible?).to eq true
-    end
-  end
-
-  context 'belonging to opponent' do
-    let(:opponent_board) { create :board, opponent?: true }
-
-    it 'holds invisible ships' do
-      expect(opponent_board.patrol_boat.visible?).to eq false
-      expect(opponent_board.destroyer.visible?).to eq false
-      expect(opponent_board.submarine.visible?).to eq false
-      expect(opponent_board.battleship.visible?).to eq false
-      expect(opponent_board.aircraft_carrier.visible?).to eq false
-    end
-  end
-
 end
