87b17b8ce5d262c6591467d97c8202f7250db357
Garett Arrowood <garettarrowood@gmail.com>
2016-01-28 09:04:35 -0500

    Game spec complete

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,17 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Game, :type => :model do
   let(:game) { create :game }
-  let(:board) { build :board }
+  let(:user_board) { create :board, opponent?: false, game: game }
   
   it 'has valid factory' do
     expect(game).to be_valid
   end
 
   context "#over?" do
-    it "returns false if game is not over" do
+    it "returns false if game has no sunk ships" do
       expect(game.over?).to eq false
     end
 
-    it "returns true if all ships are sunk on 1 board, and thus the game is over"
+    it "returns false if some but not all ships are sunk" do
+      Move.create(board: user_board, position: ActiveRecord::Point.new(1,2))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(1,3))
+      expect(user_board.ships[0].sunk?).to eq true
+      expect(game.over?).to eq false
+    end
+
+    it "returns true if all ships are sunk on one board" do
+      Move.create(board: user_board, position: ActiveRecord::Point.new(1,2))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(1,3))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(2,2))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(2,3))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(2,4))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(3,2))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(3,3))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(3,4))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(4,2))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(4,3))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(4,4))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(4,5))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(5,2))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(5,3))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(5,4))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(5,5))
+      Move.create(board: user_board, position: ActiveRecord::Point.new(5,6))
+      expect(game.over?).to eq true
+    end
   end
 end
