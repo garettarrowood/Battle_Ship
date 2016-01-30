84e24bfb9d2ac201d841898fb5aae604ab55eb1e
Garett Arrowood <garettarrowood@gmail.com>
2016-01-30 12:36:19 -0500

    Notes for future work in Multi controller

--- app/controllers/multiplayer_controller.rb
+++ app/controllers/multiplayer_controller.rb
@@ -1,16 +1,25 @@
 class MultiplayerController < ApplicationController
-  before_action :set_game
-
-  def index
-  end
+  before_action :set_game, only: [:show]
 
   def create
-    binding.pry
+    # if Game.find(multiplayer?: true, status: "available")
+      # join that game
+      # @game = that
+    # else
+      # create new game
+      # @game = that
+    #end
+    # Use Seek to determine what happens here. - some command that hits channel - maybe I don't create a game here??
+    @game = "something"
+    render js: "window.location = '/multiplayer/#{@game.id}'"
+  end
+
+  def show
   end
 
   private
 
     def set_game
-      @game = Game.find(params[:game_id])
+      @game = Game.find(params[:id])
     end
 end
