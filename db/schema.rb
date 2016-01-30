81251e61687259e8a605b0cc536573ec274c7648
Garett Arrowood <garettarrowood@gmail.com>
2016-01-30 16:06:49 -0500

    Add winner string to game

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130182616) do
+ActiveRecord::Schema.define(version: 20160130210218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160130182616) do
     t.datetime "updated_at",                       null: false
     t.boolean  "multiplayer?", default: false
     t.string   "status",       default: "pending"
+    t.string   "winner"
   end
 
   create_table "moves", force: :cascade do |t|
