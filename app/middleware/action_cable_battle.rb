27a96f990fb4eee7521ca459bc5afa2aff7484a8
Garett Arrowood <garettarrowood@gmail.com>
2016-02-06 17:12:00 -0500

    Change Websocket location

--- app/middleware/action_cable_battle.rb
+++ app/middleware/action_cable_battle.rb
@@ -4,7 +4,7 @@ class ActionCableBattle
   end
 
   def call(env)
-    if Faye::WebSocket.websocket?(env)
+    if ::WebSocket::Driver.websocket?(env)
       ActionCable.server.call(env)
     else
       @app.call(env)
