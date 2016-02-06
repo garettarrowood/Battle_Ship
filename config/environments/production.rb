689adebb99d477bd0d214c8764fe8216f7471f63
Garett Arrowood <garettarrowood@gmail.com>
2016-02-06 18:32:22 -0500

    More production experiments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://play-battleship.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://play-battleship.herokuapp.com', /http:\/\/play-battleship.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
