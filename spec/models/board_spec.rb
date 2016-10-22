5f40310eb903325bbc9e081bbb87b807105999d1
Garett Arrowood <garettarrowood@gmail.com>
2016-10-22 12:37:45 -0400

    Better format specs

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,18 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Board, :type => :model do
   let(:board) { create :board }
-  # let(:patrol_boat) { build :patrol_boat }
-  # let(:destroyer) { build :destroyer }
-  # let(:submarine) { build :submarine }
-  # let(:battleship) { build :battleship }
-  # let(:aircraft_carrier) { build :aircraft_carrier }
   
   it 'has valid factories with 5 placed ships' do
     expect(board).to be_valid
     expect(board.ships.size).to eq 5
   end
-
-  # it "e" do
-  #   binding.pry
-  # end
 end
