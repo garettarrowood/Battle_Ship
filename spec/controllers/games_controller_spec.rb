29541b3b54e21978ef1c006fd6b7ea3a8324fe80
Garett Arrowood <garettarrowood@gmail.com>
2016-10-23 19:26:06 -0400

    Add to GamesController spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe GamesController, :type => :controller do
     log_in @user
   end
 
-  context "GET index" do
+  context "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
       expect(response).to be_success
@@ -20,9 +20,25 @@ RSpec.describe GamesController, :type => :controller do
     end
 
     it "sets game if one exists on user" do
+      get :index
+      expect(assigns(:game)).to be nil
+
       game = @user.games.create
       get :index
       expect(assigns(:game).id).to be game.id
+    end
+  end
+
+  context "GET #new" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :new
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index template" do
+      get :new
+      expect(response).to render_template("new")
     end
   end
 end
