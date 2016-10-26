449a8f1fb66fbcba8c1d7f9dbe5a3ce6d4d016af
Garett Arrowood <garettarrowood@gmail.com>
2016-10-25 22:21:48 -0400

    Add POST #create tests to GameController spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe GamesController, :type => :controller do
       expect(response).to render_template("index")
     end
 
-    it "sets game if one exists on user" do
+    it "assigns game if one exists on user" do
       get :index
       expect(assigns(:game)).to be nil
 
@@ -39,6 +39,48 @@ RSpec.describe GamesController, :type => :controller do
     it "renders the index template" do
       get :new
       expect(response).to render_template("new")
+    end
+  end
+
+  context "POST #create" do
+    params =
+      { "ships"=>
+        { "0"=>
+          { "length"=>"2",
+            "classification"=>"patrol-boat",
+            "positions"=>["5-3", "5-4"]
+          },
+          "1"=>
+          { "length"=>"3",
+            "classification"=>"destroyer",
+            "positions"=>["7-7", "7-8", "7-9"]
+          },
+          "2"=>
+          { "length"=>"3",
+            "classification"=>"submarine",
+            "positions"=>["6-4", "6-5", "6-6"]
+          },
+          "3"=>
+          { "length"=>"4",
+            "classification"=>"battleship",
+            "positions"=>["4-7", "4-8", "4-9", "4-10"]
+          },
+          "4"=>
+          { "length"=>"5",
+            "classification"=>"aircraft-carrier",
+            "positions"=>["2-2", "2-3", "2-4", "2-5", "2-6"]
+          }
+        }
+      }
+
+    it "assigns game" do
+      post :create, xhr: true, params: params
+      expect(assigns(:game)).to be_instance_of Game
+    end
+
+    it "creates SetupNewGame PORO" do
+      expect(SetupNewGame).to receive(:new).and_call_original
+      post :create, xhr: :true, params: params
     end
   end
 end
