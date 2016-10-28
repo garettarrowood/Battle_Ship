0389f71d5b0ccdf5bf98be91d8eb60aebfa4c53e
Garett Arrowood <garettarrowood@gmail.com>
2016-10-27 21:19:33 -0400

    Add moves_made method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :games, join_table: :users_games
+
+  def moves_made(game)
+    game.boards.where.not(owner: "#{id}")[0].moves.size
+  end
 end
