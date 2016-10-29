4dd062eeb95149b16aecd4368eb107d392ed4f57
Garett Arrowood <garettarrowood@gmail.com>
2016-10-29 11:47:13 -0400

    Add many user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,40 @@ class User < ApplicationRecord
   has_and_belongs_to_many :games, join_table: :users_games
 
   def moves_made(game)
-    game.boards.where.not(owner: "#{id}")[0].moves.size
+    game.boards.where.not(owner: "#{id}").first.moves.size
+  end
+
+  def create_multiplayer_game(ships)
+    game = games.create(multiplayer?: true)
+    SetupNewGame.new(ships, game, self).run!
+    game
+  end
+
+  def join_multiplayer_game(ships, game)
+    game.status = "ongoing"
+    game.users << self
+    game.save
+
+    SetupNewGame.new(ships, game, self).run!
+    broadcast_start(game)
+  end
+
+  def opponent_id(game)
+    game.users.where.not(id: id).first.id
+  end
+
+  def last_game
+    games.max
+  end
+
+private
+
+  def broadcast_start(game)
+    ActionCable.server.broadcast(
+      "battleship:#{opponent_id(game)}",
+      { action: "go first",
+        name: email.split('@').first
+      }
+    )
   end
 end
