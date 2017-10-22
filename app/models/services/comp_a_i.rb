262fe0770d1a921ae2742d375f4a0f2f5eae9e50
Garett Arrowood <garettarrowood@gmail.com>
2017-10-22 15:55:29 -0400

    Cleanup comp_a_i

--- app/models/services/comp_a_i.rb
+++ app/models/services/comp_a_i.rb
@@ -1,69 +1,81 @@
 class CompAI
-
-  x = 1.0
-  @@Full_board = []
-  10.times do
-    y = 1.0
-    10.times do
-      @@Full_board << ActiveRecord::Point.new(x,y)
-      y+=1
+  def self.full_board # rubocop:disable Metrics/MethodLength
+    @full_board ||= begin
+      x = 1.0
+      board = []
+      10.times do
+        y = 1.0
+        10.times do
+          board << ActiveRecord::Point.new(x, y)
+          y += 1
+        end
+        x += 1
+      end
+      board
     end
-    x+=1
   end
 
   def initialize(board)
     @board = board
-    @available_positions = @@Full_board - @board.moves.map(&:position)
+    @available_positions = CompAI.full_board - @board.moves.map(&:position)
   end
 
-  def new_move # returns a position
+  # returns a position
+  def new_move
     case analyze
-    when "random"
+    when 'random'
       @available_positions.sample
-    when "educated guess"
-      return left(@last_hit.position) if @available_positions.include?(left(@last_hit.position))
-      return down(@last_hit.position) if @available_positions.include?(down(@last_hit.position))
-      return right(@last_hit.position) if @available_positions.include?(right(@last_hit.position))
-      return up(@last_hit.position) if @available_positions.include?(up(@last_hit.position))
-      second_last = @board.damaging_moves.sort_by { |move| move.id }[-2].position
-      return left(second_last) if @available_positions.include?(left(second_last))
-      return down(second_last) if @available_positions.include?(down(second_last))
-      return right(second_last) if @available_positions.include?(right(second_last))
-      return up(second_last) if @available_positions.include?(up(second_last))
-      return @available_positions.sample
+    when 'educated guess'
+      guess = guess_based_on(@last_hit.position)
+      return guess if guess.present?
+      guess = guess_based_on(damaging_move(-2).position)
+      return guess if guess.present?
+      @available_positions.sample
     end
   end
 
-private
+  private
+
+  def damaging_move(index)
+    @board.damaging_moves.sort_by(&:id)[index]
+  end
+
+  def guess_based_on(hit)
+    return left(hit) if @available_positions.include?(left(hit))
+    return down(hit) if @available_positions.include?(down(hit))
+    return right(hit) if @available_positions.include?(right(hit))
+    return up(hit) if @available_positions.include?(up(hit))
+    nil
+  end
 
   def analyze
-    return "random" if @board.damaging_moves.length == 0
-    @last_hit = @board.damaging_moves.sort_by { |move| move.id }.last
-    return "random" if @board.sinking_move?(@last_hit)
-    "educated guess"
+    return 'random' if @board.damaging_moves.empty?
+    @last_hit = damaging_move(-1)
+    return 'random' if @board.sinking_move?(@last_hit)
+    'educated guess'
   end
 
   def up(position)
     x = position.x
     y = position.y - 1
-    y >= 1 ? ActiveRecord::Point.new(x,y) : false
+    y >= 1 ? ActiveRecord::Point.new(x, y) : false
   end
 
   def down(position)
     x = position.x
     y = position.y + 1
-    y <= 10 ? ActiveRecord::Point.new(x,y) : false
+    y <= 10 ? ActiveRecord::Point.new(x, y) : false
   end
 
   def left(position)
     x = position.x - 1
-    y = position.y 
-    x >= 1 ? ActiveRecord::Point.new(x,y) : false
+    y = position.y
+    x >= 1 ? ActiveRecord::Point.new(x, y) : false
   end
 
   def right(position)
     x = position.x + 1
     y = position.y
-    x <= 10 ? ActiveRecord::Point.new(x,y) : false
-  end 
+    x <= 10 ? ActiveRecord::Point.new(x, y) : false
+  end
 end
