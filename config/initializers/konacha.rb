ef1ce6b6260b5ce3aaa74855300371e96ecddcb2
Garett Arrowood <garettarrowood@gmail.com>
2017-10-22 20:15:33 -0400

    More lint fixes

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,6 +1,10 @@
-Konacha.configure do |config|
-  config.spec_dir     = "spec/javascripts"
-  config.spec_matcher = /_spec\.|_test\./
-  config.stylesheets  = %w(application)
-  config.driver       = :selenium
-end if defined?(Konacha)
+# frozen_string_literal: true
+
+if defined?(Konacha)
+  Konacha.configure do |config|
+    config.spec_dir     = 'spec/javascripts'
+    config.spec_matcher = /_spec\.|_test\./
+    config.stylesheets  = %w[application]
+    config.driver       = :selenium
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
 
@@ -7,17 +9,17 @@ Rails.application.routes.draw do
 
   authenticate :user do
     root to: 'games#index'
-    resources :games, only: [:index, :new, :create, :show] do
-      post '/move', to: "games#move"
-      put '/apply_win', to: "games#apply_win"
-      put '/apply_loss', to: "games#apply_loss"
-      get '/won', to: "games#won"
-      get '/lost', to: "games#lost"
+    resources :games, only: %i[index new create show] do
+      post '/move', to: 'games#move'
+      put '/apply_win', to: 'games#apply_win'
+      put '/apply_loss', to: 'games#apply_loss'
+      get '/won', to: 'games#won'
+      get '/lost', to: 'games#lost'
     end
-    resources :multiplayer, only: [:show, :create] do 
-      get '/opponent-forfeit', to: "multiplayer#opponent_forfeit"
+    resources :multiplayer, only: %i[show create] do
+      get '/opponent-forfeit', to: 'multiplayer#opponent_forfeit'
     end
-    get '/disconnected', to: "multiplayer#disconnected"
+    get '/disconnected', to: 'multiplayer#disconnected'
   end
 
   # Serve websocket cable requests in-process
